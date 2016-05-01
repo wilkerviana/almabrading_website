961fe9b7ecea692d7b7da8ba8d761c5c3e7ed36a
Wilker Viana <wilker.info@gmail.com>
2016-04-30 23:57:24 -0300

    Release 1.0 website adaptive for mobile,tablet and Desktop

--- public/js/maintools.js
+++ public/js/maintools.js
@@ -1,12 +1,12 @@
 var block = function(){
 	var blockAnimate = $('.block-unit-animate');
 	$(blockAnimate).mouseover(function(){
-		$(this).addClass('currentAnimate');
+		$(this).removeClass('offAnimate').addClass('currentAnimate');
 		var childAnimate = $('.currentAnimate').find('.intro__resume , .service__list');
 		$(childAnimate).addClass('currentChild');
 	});
 	$(blockAnimate).mouseleave(function(){
-		$(this).removeClass('currentAnimate');
+		$(this).addClass('offAnimate').removeClass('currentAnimate');
 		$(this).find('.intro__resume , .service__list').removeClass('currentChild');
 	});
 }
